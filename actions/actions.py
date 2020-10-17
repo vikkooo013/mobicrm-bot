65f524ecf61f533fe752a242fce2b5179d2a4117
Harshit <hrsht.agarwal2@gmail.com>
2020-10-17 12:58:59 +0530

    Action and intent fix

--- actions/actions.py
+++ actions/actions.py
@@ -6,27 +6,62 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from typing import Any, Text, Dict, List
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
+
+
+class ActionCrm(Action):
+
+    def name(self) -> Text:
+        return "action_crm"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]):
+            choice = tracker.get_slot('inform')
+            requirement = choice
+            response = """Oh good! We have the ideal {} tucked in serene and landscape setting for you.""".format (requirement)
+            dispatcher.utter_message(response)
+            return [SlotSet('inform', requirement)]
+# class crmAction(Action):
+
+#     def name(self):
+#         return 'action_crm'
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         choice = tracker.get_slot('inform')
+        
+#         requirement = choice
+#         response = """Oh good! We have the ideal {} tucked in serene and landscape setting for you.""".format (requirement)
+       
+#         dispatcher.utter_message(response)
+#         return [SlotSet('inform', requirement)]
+
+
 # class ActionHelloWorld(Action):
-#
+
 #     def name(self) -> Text:
 #         return "action_hello_world"
-#
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[D - rule: Say goodbye anytime the user says goodbye
-    steps:
-      - intent: goodbye
-      - action: utter_goodbye
+#     steps:
+#       - intent: goodbye
+#       - action: utter_goodbye
 
-  ict[Text, Any]]:
-#
+#   ict[Text, Any]]:
+# #
 #         dispatcher.utter_message(text="Hello World!")
-#
+
 #         return []
